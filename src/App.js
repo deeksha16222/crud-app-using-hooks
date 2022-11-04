b1b9c34fc5ee5963df4ce692356813bcd14c3e10
unknown <deekshabisht16@gmail.com>
2022-11-04 15:36:30 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+import React, { useState } from "react";
 import "./App.css";
 import UserTable from "./tables/UserTable";
 
 function App() {
+  const usersData = [
+    { id: "1", name: "Deeksha", email: "db@gmail.com" },
+    { id: "2", name: "Vineet", email: "vt@gmail.com" },
+    { id: "3", name: "Krishna", email: "krish@gmail.com" },
+  ];
+  const [data, setData] = useState(usersData);
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -12,7 +18,7 @@ function App() {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable/>
+          <UserTable users={data} />
         </div>
       </div>
     </div>
--- src/tables/UserTable.js
+++ src/tables/UserTable.js
@@ -1,29 +1,35 @@
 import React from "react";
 
-export default function UserTable() {
+export default function UserTable(props) {
   return (
     <div>
       <table>
         <thead>
           <tr>
             <th>Name</th>
-            <th>Username</th>
+            <th>Email</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Name data</td>
-            <td>Username data</td>
-            <td>
-              <button className="button muted-button">Edit</button>
-              <button className="button muted-button">Delete</button>
-            </td>
-          </tr>
+          {props.users.length > 0 ? (
+            props.users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button className="button muted-button">Edit</button>
+                  <button className="button muted-button">Delete</button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={3}>No users</td>
+            </tr>
+          )}
         </tbody>
       </table>
-
-      
     </div>
   );
 }
