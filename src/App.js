6ee5bf613ae7659224e56949ed849f0afbaf1b74
unknown <deekshabisht16@gmail.com>
2022-11-04 14:43:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
